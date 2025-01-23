f3b1cf407e7741943bd822ca01b0d373774e3b0d
Alex <bcnzap@gmail.com>
2025-01-23 15:00:11 +0100

    feat: add 2.8 the phonebook step3

--- part-2/part2b/src/App.jsx
+++ part-2/part2b/src/App.jsx
@@ -2,32 +2,42 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '39-44-5323523' }
   ])
   const [newName, setNewName] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newNumber,
       important: Math.random() < 0.5,
       id: String(persons.length + 1),
+
     }
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
-  
+
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const personsToShow = showAll
     ? persons
     : persons.filter(person => person.important === true)
- 
 
+
+  
   return (
     <div>
       <h2>Phonebook</h2>
@@ -37,21 +47,40 @@ const App = () => {
             value={newName}
             onChange={handlePersonChange}
           />
+
         </div>
+
+        <div >
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
+
+
       <h2>Numbers</h2>
       <div>
-       <button onClick={() => setShowAll(!showAll)}>
-        show{showAll }
-       </button>
+        <button onClick={() => setShowAll(!showAll)}>
+          show{showAll}
+        </button>
       </div>
+      <div>
 
-      {personsToShow.map(person =>
-        <h2 key={person.name}>{person.name}</h2>
-      )}
+
+
+
+        {personsToShow.map(person =>
+          <h2 key={person.name}>{person.name} {person.number}</h2>
+        )}
+
+
+      </div>
 
       {/* {newName !== '' &&
         <p>{newName}</p>} */}
